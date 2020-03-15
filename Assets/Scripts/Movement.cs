310c86c8603e2390a2556bb8524623c4898dd738
Mezmaster <44645134+Mezmaster@users.noreply.github.com>
2020-03-15 20:04:11 +0000

    Refactoring
    -New export Jag model implemented
    -Suspension hierarchy adjusted

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody leftrear;
+    [SerializeField]
+    Rigidbody rightrear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            leftrear.AddTorque(Vector3.right * 5000);
+            rightrear.AddTorque(Vector3.right * 5000);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            leftrear.AddTorque(Vector3.right * -5000);
+            rightrear.AddTorque(Vector3.right * -5000);
+        }
+    }
+}
