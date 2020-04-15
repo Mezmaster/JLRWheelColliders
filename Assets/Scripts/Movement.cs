737c43ffa790b601aa6da082e148e5cc2d23d6ae
Mezmaster <44645134+Mezmaster@users.noreply.github.com>
2020-04-15 18:09:33 +0100

    New model, steering changes, suspension changes

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,26 +11,57 @@ public class Movement : MonoBehaviour
     Rigidbody rightrear;
 
     [SerializeField]
+    Rigidbody leftfront;
+
+    [SerializeField]
+    Rigidbody rightfront;
+
+    [SerializeField]
     Rigidbody mainbody;
+
+    float steeringAngle;
+    float steeringMagnitude;
+    float dampeningForce;
+
+    [Range(0, 50000)]
+    public int steeringForce;
+
+    [Range(0, 50000)]
+    public int drivingForce;
 
     // Start is called before the first frame update
     void Start()
     {
-        mainbody.centerOfMass = Vector3.back;
+        mainbody.centerOfMass = Vector3.up * 0.5f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            leftrear.AddTorque(Vector3.right * 25000);
-            rightrear.AddTorque(Vector3.right * 25000);
+            leftrear.AddTorque(Vector3.right * drivingForce);
+            rightrear.AddTorque(Vector3.right * drivingForce);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            leftrear.AddTorque(Vector3.right * -25000);
-            rightrear.AddTorque(Vector3.right * -25000);
+            leftrear.AddTorque(Vector3.right * -drivingForce);
+            rightrear.AddTorque(Vector3.right * -drivingForce);
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            steeringAngle -= 3.0f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            steeringAngle += 3.0f;
+        }
+        steeringAngle = Mathf.Clamp(steeringAngle, -45, 45);
+        steeringMagnitude = Mathf.Sin(steeringAngle) * steeringForce;
+        dampeningForce = (steeringForce - (Mathf.Cos(steeringAngle) * steeringForce));
+        leftfront.AddForce(Vector3.right * steeringMagnitude);
+        rightfront.AddForce(Vector3.right * steeringMagnitude);
+        leftrear.AddForce(Vector3.forward * -dampeningForce);
+        rightrear.AddForce(Vector3.forward * -dampeningForce);
     }
 }
