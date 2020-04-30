785f78b6b9709d0098d9b1d4c16536a320b3493c
Mezmaster <44645134+Mezmaster@users.noreply.github.com>
2020-04-30 22:14:11 +0100

    Implemented graphing and csv writing

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,23 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField]
-    Rigidbody leftrear;
+    Rigidbody leftRearWheel;
 
     [SerializeField]
-    Rigidbody rightrear;
+    Rigidbody rightRearWheel;
 
     [SerializeField]
-    Rigidbody leftfront;
+    Rigidbody leftFrontWheel;
 
     [SerializeField]
-    Rigidbody rightfront;
+    Rigidbody rightFrontWheel;
 
     [SerializeField]
-    Rigidbody mainbody;
+    Rigidbody mainBody;
+
+    [SerializeField]
+    GameObject leftRearCompressionObject;
+
+    [SerializeField]
+    GameObject rightRearCompressionObject;
+
+    [SerializeField]
+    GameObject leftFrontCompressionObject;
+
+    [SerializeField]
+    GameObject rightFrontCompressionObject;
+
+    float leftRearCompression;
+    float rightRearCompression;
+    float leftFrontCompression;
+    float rightFrontCompression;
 
     float steeringAngle;
     float steeringMagnitude;
@@ -30,27 +49,69 @@ public class Movement : MonoBehaviour
     [Range(0, 100000)]
     public int drivingForce;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Image leftFrontCompressionBar;
+
+    [SerializeField]
+    Image leftRearCompressionBar;
+
+    [SerializeField]
+    Image rightFrontCompressionBar;
+
+    [SerializeField]
+    Image rightRearCompressionBar;
+
+    /*
+    List<float> leftRearCompressionList;
+    List<float> rightRearCompressionList;
+    List<float> leftFrontCompressionList;
+    List<float> rightFrontCompressionList;
+    List<float> timeStampList;
+    List<float> steeringAngleList;
+    */
+
+    //Start is called before the first frame update
+    void Awake()
     {
-        mainbody.centerOfMass = Vector3.up * 0.5f;
+        mainBody.centerOfMass = Vector3.up * 0.5f;
+        using (StreamWriter fileWriter = new StreamWriter("telemetryData.csv", false))
+        {
+            fileWriter.WriteLine("unixTime" + "," + "leftRearCompression" + "," + "rightRearCompression" +
+                "," + "leftFrontCompression" + "," + "rightFrontCompression" + "," + "steeringAngle");
+        }
+    }
+    
+
+    void Update()
+    {
+        using (StreamWriter fileWriter = new StreamWriter("telemetryData.csv", true))
+        {
+            fileWriter.WriteLine(System.DateTime.Now.Ticks.ToString("##################") + "," + leftRearCompression + "," + rightRearCompression +
+                "," + leftFrontCompression + "," + rightFrontCompression + "," + steeringAngle);
+        }
+        leftFrontCompressionBar.fillAmount = leftFrontCompression;
+        leftRearCompressionBar.fillAmount = leftRearCompression;
+        rightFrontCompressionBar.fillAmount = rightFrontCompression;
+        rightRearCompressionBar.fillAmount = rightRearCompression;
     }
 
-    // Update is called once per frame
+    //FixedUpdate has a 50Hz refresh rate, being called 50 times a second
     void FixedUpdate()
     {
+		//This bit drives
         if (Input.GetKey(KeyCode.W))
         {
             acceleration += 0.1f;
-            leftrear.AddTorque(Vector3.right * (drivingForce * acceleration));
-            rightrear.AddTorque(Vector3.right * (drivingForce * acceleration));
+            leftRearWheel.AddTorque(Vector3.right * (drivingForce * acceleration));
+            rightRearWheel.AddTorque(Vector3.right * (drivingForce * acceleration));
         }
         if (Input.GetKey(KeyCode.S))
         {
             acceleration -= 0.1f;
-            leftrear.AddTorque(Vector3.right * (drivingForce * acceleration));
-            rightrear.AddTorque(Vector3.right * (drivingForce * acceleration));
+            leftRearWheel.AddTorque(Vector3.right * (drivingForce * acceleration));
+            rightRearWheel.AddTorque(Vector3.right * (drivingForce * acceleration));
         }
+		//This bit steers
         if (Input.GetKey(KeyCode.A))
         {
             steeringAngle += 5.0f;
@@ -59,24 +120,36 @@ public class Movement : MonoBehaviour
         {
             steeringAngle -= 5.0f;
         }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            drivingForce += 200000;
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            drivingForce -= 200000;
-        }
+		//This bit does complicated steering calculation maff
         acceleration = Mathf.Clamp(acceleration, -1, 1);
-        Debug.Log(acceleration);
         steeringAngle = Mathf.Clamp(steeringAngle, -45, 45);
+        CalculateSteering();
+        CalculateSuspensionCompression();
+        Debug.Log(Time.time);
+    }
+    void CalculateSuspensionCompression()
+    {
+        leftRearCompression = Vector3.Distance(leftRearCompressionObject.transform.position, leftRearWheel.transform.position);
+        rightRearCompression = Vector3.Distance(rightRearCompressionObject.transform.position, rightRearWheel.transform.position);
+        leftFrontCompression = Vector3.Distance(leftFrontCompressionObject.transform.position, leftFrontWheel.transform.position);
+        rightFrontCompression = Vector3.Distance(rightFrontCompressionObject.transform.position, rightFrontWheel.transform.position);
+        Debug.Log(leftRearCompression);
+        Debug.Log(rightRearCompression);
+        Debug.Log(leftFrontCompression);
+        Debug.Log(rightFrontCompression);
+    }
+    void CalculateSteering()
+    {
         steeringMagnitude = Mathf.Sin(steeringAngle) * steeringForce;
         dampeningForce = (drivingForce - (Mathf.Cos(steeringAngle) * drivingForce));
         dampeningForce = Mathf.Clamp(dampeningForce, 0, drivingForce * 0.75f);
-        leftfront.AddForce(Vector3.left * steeringMagnitude);
-        rightfront.AddForce(Vector3.left * steeringMagnitude);
-        leftrear.AddTorque((Vector3.forward * -dampeningForce) / 2);
-        rightrear.AddTorque((Vector3.forward * -dampeningForce) / 2);
+        leftFrontWheel.AddForce(Vector3.left * steeringMagnitude);
+        rightFrontWheel.AddForce(Vector3.left * steeringMagnitude);
+        leftRearWheel.AddTorque((Vector3.forward * -dampeningForce) / 2);
+        rightRearWheel.AddTorque((Vector3.forward * -dampeningForce) / 2);
+    }
+    void RecordTelemetryData()
+    {
 
     }
 }
